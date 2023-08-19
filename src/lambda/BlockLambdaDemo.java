17ce173ec8657cbaafbfe60da8ac9ad497e925f6
DivyamKumar3 <divyamsunny@yahoo.co.in>
2023-08-19 16:32:10 +0530

    lerna lambda

---
+++ src/lambda/BlockLambdaDemo.java
@@ -0,0 +1,17 @@
+package lambda;
+public class BlockLambdaDemo {
+
+    public static void main (String arg[]){
+
+
+        NumericFunction factorial =(n) -> {
+            int result = 1;
+            for(int i=1; i<=n; i++)
+                result =i * result;
+            return result;
+        };
+
+
+        System.out.println("factorial of 5 is " + factorial.func(5));
+    }
+}
